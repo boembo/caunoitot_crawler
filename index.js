36e7a65601dfae7455e4357f1cdf0c210baff1cd
hi <caubebinhan@gmail.com>
2023-03-31 21:41:11 +0900

    OK add localStorage and Cookie from File

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-// import puppeteer library
 const puppeteer = require('puppeteer')
+const fs = require('fs').promises;
 
 async function run(){
     // First, we must launch a browser instance
     const browser = await puppeteer.launch({
-        // Headless option allows us to disable visible GUI, so the browser runs in the "background"
-        // for development lets keep this to true so we can see what's going on but in
-        // on a server we must set this to true
         headless: true,
-        // This setting allows us to scrape non-https websites easier
         ignoreHTTPSErrors: true,
+        //executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
+       // args: ["--lang=en-US,en", '--no-sandbox', '--disable-setuid-sandbox', '--disable-extensions']
+        // userDataDir:"C:\\Users\\rin rin\\AppData\\Local\\Chromium\\User Data"
     })
-    // then we need to start a browser tab
+
     let page = await browser.newPage();
+    const cookiesString = await fs.readFile('./cookie.json');
+    const cookies = JSON.parse(cookiesString);
+
+    const localStorages = await fs.readFile('./localstorage.txt');
+    const lStorage = JSON.parse(localStorages);
+    
+    await page.evaluateOnNewDocument((data) => {
+        for (const [key, value] of Object.entries(data)) {
+            localStorage.setItem(key, value);
+        }
+      }, lStorage);
+      
+    await page.setCookie(...cookies.cookies);
     // and tell it to go to some URL
-    await page.goto('http://httpbin.org/html', {
-        waitUntil: 'domcontentloaded',
+    await page.goto('https://app.recruitery.co/jobs', {
+        waitUntil: 'networkidle0',
     });
+
     // print html content of the website
-    console.log(await page.content());
-    // close everything
+    await page.screenshot({path: 'example.png'});
+    
     await page.close();
     await browser.close();
 }
