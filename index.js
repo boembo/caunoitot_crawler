243164edc6d28c5f111e8315dbbcd2ec4e6b8f5b
Le Dinh Linh <le_dinh_linh@mut.co.jp>
2023-04-07 11:55:21 +0900

    insert mysql ok

--- index.js
+++ index.js
@@ -4,30 +4,44 @@ const mysql = require('mysql');
 const path = require('path');
 const axios = require('axios');
 
-const connection = mysql.createConnection({
+const mySqlConnection = mysql.createConnection({
   host: 'localhost',
-  user: 'username',
-  password: 'password',
-  database: 'database_name',
+  user: 'root',
+  password: '',
+  database: 'caunoitot_job',
 });
+
+const ignoreList = {
+  memo: [9323,9136],
+}
 //To convert from detail Title to DB column
-const jobContentAttributes = {
+var jobContentAttributes = {
   "Interview process": "interview_process",
   "Report to": "report_to",
   "Preferred skills and experiences": "preferred_skill",
-  "Job overview and responsibility": "job_responsibility",              
+  "Job overview and responsibility": "job_responsibility",  
+  "Required skills and experiences": "job_required_skill",
+  "Why Candidate should apply this position": "why_should_apply" 
 };
 
-const headerAttributes = {
+var companyAttributes = {
+  'Team size': 'company_team_size',
+  'Address': 'company_address',
+  'website': 'company_website',
+  'Working hours': 'company_working_hour'
+}
+
+var headerAttributes = {
   gross_month_salary: "Gross Monthly salary",
   total_vacancies: "Total vacancies:",
   level: "Level",
   employment_type: "Employment type",
-  address: "Address",
+  job_address: "Address",
   is_it_job: "Types",
-  team_size: "Team size",
+  job_team_size: "Team size",
+  //note may be multiple location : "Ha noi, Da Nang"
+  job_location: "Location",
 };
-
 
 async function run(){
     // First, we must launch a browser instance
@@ -58,8 +72,8 @@ async function run(){
     //selected Ha noi, HCM, Japan, Danang
     //Just Select Headhunting
     const startPage = 1;
-    const recruiteryURL = "https://app.recruitery.co/jobs?location=1%2C1019%2C2%2C1006%2C3&page=";
-    await page.goto('https://app.recruitery.co/jobs?page=1&location=1%2C1019%2C2%2C1006%2C3', {
+    const recruiteryURL = "https://app.recruitery.co/jobs?location=1%2C1019%2C2%2C1006%2C3&status=5&page=";
+    await page.goto('https://app.recruitery.co/jobs?page=1&location=1%2C1019%2C2%2C1006%2C3&status=5', {
         waitUntil: 'networkidle0',
     });
 
@@ -114,26 +128,59 @@ async function run(){
               behavior: 'allow', 
               downloadPath: path.resolve('./defaultDownload'),  
             });
+            //Initiate data for each Source
+            var jobData = {
+              source_site: "Recruitery",
+              source_id: jobId,
+            };
+
+            const jobTitle = await page.$eval(`h1[class*="title-section_title-section__"].ant-typography `, el => el.textContent);
+            console.log(jobTitle);
+            jobData.title = jobTitle;
+
+            const jobRewardSelector = 'span[class*="job-detail-header_job-detail-header__reward-number"]';
+            const jobReward = await page.$eval(`${jobRewardSelector}`, el => el.textContent);
+            console.log(jobReward);
+            jobData.reward = jobReward;
+
+            //GET JOB TAGS
+            const tagElements = await page.$$eval(`span.ant-tag`, tags => {
+              return tags.map(tag => {
+                return tag.textContent.trim();
+              });
+            });
+            console.log("TAG elements");
+            console.log(tagElements);
+
+            jobData.tags = JSON.stringify(tagElements);
     
             //GET JOB HEADER ATTRIBUTES
             for (const [key, headerAttribute] of Object.entries(headerAttributes)) {
                 const element = await page.$x(`//div[@class='ant-descriptions-item-container']//div[contains(text(), '${headerAttribute}')]`);
+                let headerContent = '';
                 if (element.length > 0) {
                   const nextElement = await element[0].$x('following-sibling::div[1]');
                   if (nextElement.length > 0) {
-                    const content = await page.evaluate(el => el.textContent, nextElement[0]);
-                    console.log(`Content of the div next to '${headerAttribute}':`, content.trim());
+                    headerContent = await page.evaluate(el => el.textContent, nextElement[0]);
+                    console.log(`Content of the div next to '${headerAttribute}':`, headerContent.trim());
                   } else {
                     console.log(`Div next to '${headerAttribute}' not found!`);
                   }
                 } else {
                   console.log(`Div containing '${headerAttribute}' not found!`);
                 }
+
+                jobData[key] = headerContent.trim();
             }
+            console.log("job Data here");
+            console.log(jobData);
 
             const jobMemoElement = await page.$('div.ant-alert-message div[class*=job-detail_job-detail__notice]');
-            const jobMemo = await page.evaluate(element => element.textContent, jobMemoElement);
-            console.log("job memo  " + jobMemo);
+            if(jobMemoElement.length > 0) {
+              const jobMemo = await page.evaluate(element => element.textContent, jobMemoElement);
+              console.log("job memo  " + jobMemo);
+              jobData.memo = jobMemo;
+            };
             //END JOB HEADER ATTRIBUTES
 
             //GET JOB BENEFIT ATTRIBUTES
@@ -142,30 +189,40 @@ async function run(){
             // Get all the h4 elements with class ant-list-item-meta-title inside the selected div
             // const benefitElements = await page.$$(`${benefitDivSelector} h4.ant-list-item-meta-title`);
             const benefitElements = await page.$$eval(`${benefitDivSelector} h4.ant-list-item-meta-title`, titles => {
-              return titles.map(title => {
-                const titleText = title.textContent.trim();
-                const nextSibling = title.nextElementSibling;
-                const nextSiblingText = nextSibling ? nextSibling.textContent.trim() : '';
-                return [titleText, nextSiblingText];
-              });
-            });
-            console.log(benefitElements);
-            //END JOB BENEFIT ATTRIBUTES
-
-            //GET JOB CONTENT 
-            const jobContentSelector = 'h3[class*="edit-summary-view_edit-summary-view__heading"]';
-            const jobContentElements = await page.$$eval(`${jobContentSelector}`, titles => {
-              const jobContents = {};
+              const benefitContents = {};
 
               titles.forEach(title => {
                 const titleText = title.textContent.trim();
                 const nextSibling = title.nextElementSibling;
                 const nextSiblingText = nextSibling ? nextSibling.textContent.trim() : '';
-                jobContents[titleText] = nextSiblingText;
+                
+                benefitContents[titleText] = nextSiblingText;
               });
-              return jobContents;
+              return benefitContents;
             });
-            //END JOB CONTENT
+            console.log(benefitElements);
+
+            jobData.benefits = JSON.stringify(benefitElements);
+            //END JOB BENEFIT ATTRIBUTES
+
+            //GET JOB CONTENT 
+            console.log("GET ALL TITL 1 E");
+
+              const jobContentSelector = 'h3[class*="edit-summary-view_edit-summary-view__heading"]';
+              const jobContentDatas = await page.$$eval(`${jobContentSelector}`, (titles, jobContentAttributes) => {
+                const jobContents = {};
+  
+                titles.forEach(title => {
+                  const titleText = title.textContent.trim();
+                  const nextSibling = title.nextElementSibling;
+                  const nextSiblingText = nextSibling ? nextSibling.textContent.trim() : '';
+                  jobContents[jobContentAttributes[titleText]] = nextSiblingText;
+                });
+
+                return jobContents;
+              }, jobContentAttributes);
+
+              jobData = Object.assign(jobData, jobContentDatas);
 
 
             //GET COMPANY DETAIL
@@ -176,6 +233,7 @@ async function run(){
             const companyDescriptionElement = await page.$('div.ant-row div.ant-col-24 p:first-child');  
             const companyDescription = await page.evaluate(element => element.textContent, companyDescriptionElement);
             console.log("company desc" + companyDescription);
+            
             //SAVE LOGO
             const companyLogoUrl = await page.$eval('div.ant-row div.ant-col-6 img', img => img.src);
             // Use axios to download the image as a buffer
@@ -190,20 +248,28 @@ async function run(){
               require('fs').writeFileSync(`img/company_logo/recruitery/${jobId}.jpg`, imageBuffer.data);
             }
 
+            jobData.company_name = companyName;
+            jobData.company_description = companyDescription;
+
             const companyDetailSelector = 'div[class*="job-description_title"]';
-            const companyDetailElements = await page.$$eval(`${companyDetailSelector}`, titles => {
+            const companyDetailData = await page.$$eval(`${companyDetailSelector}`, (titles, companyAttributes) => {
               const companyContents = {};
 
               titles.forEach(title => {
                 const titleText = title.textContent.trim();
                 const nextSibling = title.nextElementSibling;
                 const nextSiblingText = nextSibling ? nextSibling.textContent.trim() : '';
-                companyContents[titleText] = nextSiblingText;
+
+                companyContents[companyAttributes[titleText]] = nextSiblingText;
               });
+
               return companyContents;
-            });
+            }, companyAttributes);
+
+            console.log(companyDetailData);
+
+            jobData = Object.assign(jobData, companyDetailData);
             //END JOB CONTENT
-            console.log(companyDetailElements);
 
 
 
@@ -229,20 +295,27 @@ async function run(){
                 }
               }
             });
+
+
+            console.log(jobData);
             //CLICK DOWNLOAD FILE 
             await page.click('.ant-card-extra .ant-space-item:first-child'); // some button that triggers file selection
             // const downloadBtn = await page.waitForSelector('div:contains("Download JD")');
             // await downloadBtn.click();
+
+            mySqlConnection.connect(function(err) {
+              if (err) throw err;
+
+              var query = 'INSERT INTO job SET ' + mySqlConnection.escape(jobData, true);
+              mySqlConnection.query(query, function (err, result) {
+                if (err) throw err;
+                console.log("1 record inserted, ID: " + result.insertId);
+              });
+          });
           
     // }
 
-    // insert each URL into the database
-    //   anchors.forEach(href => {
-    //     connection.query(`INSERT INTO example (postname) VALUES ('${href}')`, (error, results, fields) => {
-    //       if (error) throw error;
-    //     });
-    //   });
-
+   
     // await page.close();
     // await browser.close();
 // }
